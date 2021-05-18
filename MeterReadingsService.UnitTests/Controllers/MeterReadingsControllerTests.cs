62be3dd7c866e5361d8a3f92734a2e807919908d
Terry Mace <terry.mace@bglgroup.co.uk>
2021-05-18 19:18:48 +0100

    Setup Endpoint

---
+++ MeterReadingsService.UnitTests/Controllers/MeterReadingsControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using FluentAssertions;
+using MeterReadingsService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace MeterReadingsService.UnitTests.Controllers
+{
+    class MeterReadingsControllerTests
+    {
+        [Test]
+        public void ShouldImplementControllerBase()
+        {
+            typeof(MeterReadingsController).Should().BeAssignableTo<ControllerBase>();
+        }
+
+        [Test]
+        public void ShouldBeDecoratedWithApiControllerAttribute()
+        {
+            typeof(MeterReadingsController).Should().BeDecoratedWith<ApiControllerAttribute>();
+        }
+
+
+        [Test]
+        public void ShouldBeDecoratedWithRouteAttribute()
+        {
+            typeof(MeterReadingsController).Should().BeDecoratedWith<RouteAttribute>(attr => attr.Template == "api");
+        }
+
+        [Test]
+        public void Upload_ShouldBeDecoratedWithHttpPostAttribute()
+        {
+            var uploadMethod = typeof(MeterReadingsController).Methods().Single(x => x.Name == "Upload");
+
+            uploadMethod.Should().BeDecoratedWith<HttpPostAttribute>(attr => attr.Template == "meter-reading-uploads");
+        }
+
+
+        [Test]
+        public void Upload_ShouldReturnOkResult()
+        {
+            // arrange
+            var controller = new MeterReadingsController();
+
+            // act
+            var result = controller.Upload();
+
+            // assert
+            result.Should().BeOfType<OkResult>();
+        }
+    }
+}
---
+++ MeterReadingsService/Controllers/MeterReadingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeterReadingsService.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MeterReadingsController : ControllerBase
+    {
+        [HttpPost("meter-reading-uploads")]
+        public IActionResult Upload()
+        {
+            return Ok();
+        }
+    }
+}
+
