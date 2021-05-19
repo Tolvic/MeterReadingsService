23c6ddf3530eb14e87759ef67c3668f8955b202a
Terry Mace <terry.mace@bglgroup.co.uk>
2021-05-19 23:28:39 +0100

    Set up a MeterReadingRepository

---
+++ MeterReadingsService.UnitTests/Repository/MeterReadingsRepositoryTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MeterReadingsService.Data;
+using MeterReadingsService.Models;
+using MeterReadingsService.Repositories;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace MeterReadingsService.UnitTests.Repository
+{
+    class MeterReadingsRepositoryTests
+    {
+        private InMemoryDbHelper _inMemoryDbHelper;
+        private MeterReadingServiceContext _context;
+        private MeterReadingsRepository _meterReadingsRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            SetUpInMemoryDb();
+            _meterReadingsRepository = new MeterReadingsRepository(_context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DeleteInMemoryDb();
+        }
+
+        [Test]
+        public void AddRange_AddsMeterReadingsToDatabase()
+        {
+            // arrange 
+            var dateTime = new DateTime(2021, 05, 19, 16, 42, 00);
+
+            var meterReadings = new List<MeterReading>
+            {
+                new MeterReading
+                {
+                    AccountId = 1,
+                    MeterReadValue = 12345,
+                    MeterReadingDateTime = dateTime
+                },
+                new MeterReading
+                {
+                    AccountId = 2,
+                    MeterReadValue = 12345,
+                    MeterReadingDateTime = dateTime
+                },
+            };
+
+            // act
+            _meterReadingsRepository.AddRange(meterReadings);
+
+            // assert
+            var dbEntries = _context.MeterReadings.ToList();
+
+            dbEntries.Should().BeEquivalentTo(meterReadings, options => options.Excluding(x => x.Id));
+        }
+
+        private void SetUpInMemoryDb()
+        {
+            _inMemoryDbHelper = new InMemoryDbHelper();
+            var options = new DbContextOptionsBuilder<MeterReadingServiceContext>()
+                .UseInMemoryDatabase(databaseName: "testDb")
+                .Options;
+
+            _context = new MeterReadingServiceContext(options);
+        }
+
+        private void DeleteInMemoryDb()
+        {
+            _context.Database.EnsureDeleted();
+        }
+    }
+}
---
+++ MeterReadingsService/Repositories/MeterReadingsRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using MeterReadingsService.Data;
+using MeterReadingsService.Models;
+
+namespace MeterReadingsService.Repositories
+{
+    public class MeterReadingsRepository
+    {
+        private readonly MeterReadingServiceContext _context;
+
+        public MeterReadingsRepository(MeterReadingServiceContext context)
+        {
+            _context = context;
+        }
+
+        public void AddRange(List<MeterReading> meterReadings)
+        {
+            _context.MeterReadings.AddRange(meterReadings);
+
+            _context.SaveChanges();
+        }
+    }
+}
