e8fc6f83625081af32573c8b455fbbc070f02a66
Terry Mace <terry.mace@bglgroup.co.uk>
2021-05-19 23:06:28 +0100

    Set up DB Seeding

---
+++ MeterReadingsService/Data/DataSeeder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using MeterReadingsService.Models;
+
+namespace MeterReadingsService.Data
+{
+    public class DataSeeder
+    {
+        private readonly MeterReadingServiceContext _context;
+        private const string TestAccountsFileName = "\\Data\\Test_Accounts.csv";
+
+        public DataSeeder(MeterReadingServiceContext context)
+        {
+            _context = context;
+        }
+
+        //TODO Given more time this would be tested and calls to System.IO objects, etc. would be abstracted away similar to the FileStorageService
+
+        public void Seed()
+        {
+            if (_context.Accounts.Any())
+            {
+                return;
+            }
+
+            _context.Database.EnsureCreated();
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+
+            var filePath = $"{currentDirectory}{TestAccountsFileName}";
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(nameof(filePath));
+            }
+
+            var streamReader = new StreamReader(filePath);
+
+            var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+            List<Account> records;
+
+            using (csvReader)
+            {
+                records = csvReader.GetRecords<Account>().ToList();
+            }
+
+            _context.Accounts.AddRange(records);
+
+            _context.SaveChanges();
+        }
+    }
+}
--- MeterReadingsService/Program.cs
+++ MeterReadingsService/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MeterReadingsService.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MeterReadingsService
 {
@@ -13,7 +9,20 @@ namespace MeterReadingsService
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<MeterReadingServiceContext>();
+
+                var dataSeeder = new DataSeeder(context);
+
+                dataSeeder.Seed();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
